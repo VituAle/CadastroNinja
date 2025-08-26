c8bebe42862c70ea6db4b216b895398156b2bc51
VituAle <vitor_rj@hotmail.com>
2025-08-25 22:58:49 -0300

    feat: alterar rota do controller para listar os ninjas e injetar a dependencia do repository

--- src/main/java/dev/batismodejava/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/batismodejava/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,22 @@
 package dev.batismodejava.CadastroDeNinjas.Ninjas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //Listar todos os meus Ninjas
+
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
 }
