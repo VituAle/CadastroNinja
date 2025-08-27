52d49535089fd5e5eff17327f2c03316a9f0e145
VituAle <vitor_rj@hotmail.com>
2025-08-26 23:05:41 -0300

    feat: inclui a rota de cadastro de ninja usando o metodo post.

--- src/main/java/dev/batismodejava/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/batismodejava/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -16,13 +16,19 @@ public class NinjaService {
     }
 
     //Listar todos os meus Ninjas
-
     public List<NinjaModel> listarNinjas() {
         return ninjaRepository.findAll();
     }
 
+    // Listar todos os ninjas por id
     public NinjaModel listarNinjasPorId(Long id) {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
+    }
+
+    //criar um novo ninja
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+
     }
 }
