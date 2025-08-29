a65640fb11baf8145739e3e5523ccf85d1e0370a
VituAle <vitor_rj@hotmail.com>
2025-08-28 22:56:53 -0300

    fix: tirou a responsabilidade do model e passou para o DTO

--- src/main/java/dev/batismodejava/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/batismodejava/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.batismodejava.CadastroDeNinjas.Ninjas;
 
+import dev.batismodejava.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+
+    private long id;
+    private String nome;
+    private String email;
+    private String rank;
+    private int idade;
+    private MissoesModel missoes;
+
+
 }
